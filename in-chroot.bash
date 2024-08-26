cfae8713a9a45061e475deccaab45bf26f3456fd
AMDRadeonRX6750XT <176963444+AMDRadeonRX6750XT@users.noreply.github.com>
2024-08-26 22:36:33 +0200

    internet fix

--- in-chroot.bash
+++ in-chroot.bash
@@ -27,8 +27,9 @@ pacman -S grub efibootmgr --noconfirm
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
-pacman -S xfce4 lightdm lightdm-gtk-greeter xdg-user-dirs neofetch wget git --noconfirm
+pacman -S xfce4 lightdm lightdm-gtk-greeter xdg-user-dirs neofetch wget git dhcpcd dhcpcd-ui --noconfirm
 
+systemctl enable dhcpcd.service
 systemctl enable lightdm.service
 
 xfce4-setting-manager --gtk-theme Adwaita-dark # set dark theme
